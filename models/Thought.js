c588bbbd7eae7251401bcf3c4d3efbb78803e7a9
shanep42 <107292088+shanep42@users.noreply.github.com>
2022-11-19 13:56:01 -0600

    Adds reactionCount method on thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -33,34 +33,33 @@ const thoughtSchema = new Schema(
         thoughtText: {
             type: String,
             required: true,
-            //TODO: Must be between 1 and 280 characters
+            minlength: 1,
+            maxlength: 280,
         },
         createdAt: {
             type: Date,
-            //TODO: Default value at current timestamp
-            //Getter method to format the timestamp on query
+            default: Date.now,
+            //TODO: Getter method to format the timestamp on query
         },
         username: {
             type: String,
             required: true,
         },
-        reactions: [
-            {
-                
-            }
-        ]
-            //TODO: Array of nested documents created with the reactionSchema
-        
+        reactions: [reactionSchema]
     },
     {
         toJSON: {
             virtuals: true,
+            getters: true,
         },
         id: false,
     }
 );
 
-//TODO: Create a virtual called reactionCount that retrieves the length of the thought's reactions array on query
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
 
 const Thought = model('thought', thoughtSchema);
 
